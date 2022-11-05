54ad26904a29e0f87da7a3178e75e7c1c07f34c2
Evan Kleiner <ekleiner@synap.io>
2022-11-05 15:42:00 -0700

    Datepicker in modal issue reproduced

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ionic Issue Reproduction Repository
+
+This is a simple ionic repository to reproduce an issue with the `<ion-datetime>`
+component when it is included in an `<ion-modal>` component and opened on click of an `<ion-input type="date">` component.
+It was generated with the Ionic "Getting Started Wizard" (https://ionicframework.com/start#basics).
+
+Issue: https://github.com/ionic-team/ionic-framework/issues/26234
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  @ViewChild('modalElement') public modal: IonModal;
 
   constructor() {}
 
+  public openModal(): void {
+    this.modal.present();
+  }
 }
