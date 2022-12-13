5b7cfd8bc44ddb314d3f227e7b2f3cc3afeff0f4
Evan Kleiner <ekleiner@synap.io>
2022-12-12 17:48:48 -0800

    IonInput setFocus() bug reproduced

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  AfterViewInit,
+  ViewChild,
+} from '@angular/core';
+import { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-explore-container',
   templateUrl: './explore-container.component.html',
   styleUrls: ['./explore-container.component.scss'],
 })
-export class ExploreContainerComponent implements OnInit {
+export class ExploreContainerComponent implements OnInit, AfterViewInit {
+  @ViewChild('myInput') myInput: IonInput;
   @Input() name: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
 
+  public ngAfterViewInit(): void {
+    console.log('Running without timeout');
+    console.log(this.myInput.setFocus);
+    this.myInput.setFocus();
+  }
+
+  // Still doesn't work on my machine
+  // public ngAfterViewInit(): void {
+  //   setTimeout(() => {
+  //     console.log('Running with 5 second timeout');
+  //     console.log(this.myInput.setFocus);
+  //     this.myInput.setFocus();
+  //   }, 5000);
+  // }
 }
