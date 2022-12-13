1da8c7e79dca4989a3a2be079766713a6006e31b
Evan Kleiner <ekleiner@synap.io>
2022-12-13 13:13:14 -0800

    setFocus() moved to Ionic lifecycle hook

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  @ViewChild('myInput') myInput: IonInput;
 
   constructor() {}
 
+  public ionViewDidEnter(): void {
+    console.log('Running without timeout');
+    console.log(this.myInput.setFocus);
+    this.myInput.setFocus();
+    console.log('hi');
+  }
 }
